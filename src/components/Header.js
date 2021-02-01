d72ba9559868dc921f5c8cc82eec9790fc5f38d3
OrrinBlake <orrinblake@gmail.com>
2021-02-01 15:34:32 +0000

    Add Project
    Add meme generator where users can add a top text and bottom text to
    create memes.

---
+++ src/components/Header.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <div>
+            <header>
+                <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" alt="Broken?"/>
+                <p>Memeulator 2000</p>
+            </header>
+        </div>
+    );
+}
+
+export default Header;
