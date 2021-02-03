9fdb7149539ed5bdd859f93baf5278d994fc41d3
OrrinBlake <orrinblake@gmail.com>
2021-02-03 19:55:40 +0000

    Add troll face header image locally

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
+import trollFace from '../images/Trollface.png';
 
 const Header = () => {
     return (
         <div>
             <header>
-                <img src="http://www.pngall.com/wp-content/uploads/2016/05/Trollface.png" alt="Broken?"/>
+                <img src={trollFace} alt="Broken?"/>
                 <p>Memeulator 2000</p>
             </header>
         </div>
